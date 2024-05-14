88456bfd105d828afa68897cfced286145a8daa4
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-05-14 10:56:23 -0300

    Chatbot working... ran out of prompts for my plan

--- backend.py
+++ backend.py
@@ -1,2 +1,26 @@
+import openai
+import os
+
+API_KEY = os.getenv("CHATGPT_STUDY_KEY")
+
+
 class Chatbot:
-    pass
+    def __init__(self):
+        self.chatbot = openai.OpenAI(api_key=API_KEY)
+
+        openai.api_key = API_KEY
+
+    def get_response(self, user_input):
+        response = self.chatbot.completions.create(
+            model="davinci-002",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        )
+        return response
+
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    test_response = chatbot.get_response("Give me a riddle")
+    print(test_response)
--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import sys
-
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
+from backend import Chatbot
+import threading
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.chatbot = Chatbot()
+
         # Window config
         self.setFixedSize(730, 400)
 
         # Chat Area
         self.chat_area = QTextEdit(self)
-        self.chat_area.setGeometry(10, 10, 600, 340)
+        self.chat_area.setGeometry(10, 10, 710, 340)
         self.chat_area.setReadOnly(True)
 
         # Input Field
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 360, 600, 30)
+        self.input_field.returnPressed.connect(self.send_message)
 
         # Send Button
         self.button = QPushButton("Send", self)
         self.button.setGeometry(620, 360, 100, 30)
-        self.button.clicked.connect("""""")
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"You: {user_input}")
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_response, args=(user_input, ))
+        thread.start()
+
+    def get_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"Bot: {response}")
 
 
 app = QApplication(sys.argv)
 main_window = ChatbotWindow()
 sys.exit(app.exec())
-
